89f73275681b8a5f2c3ef6c0a64de3f8d20b8195
Jose Monteiro <boanerges.avila@gmail.com>
2023-07-05 16:43:28 -0300

    End of controller module

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,34 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    protected $request;
+
+    public function __construct(Request $request){
+        $this->$request = $request;
+
+        // Posso aplicar o middleware no contrutor para todo o controller
+        //$this->middleware('auth');
+
+        // Posso aplicar o middleware no contrutor para um metodo específico do controller
+        //$this->middleware('auth')->only('create');
+
+        // Posso aplicar o middleware no contrutor para varios metodos específicos do controller
+        /*$this->middleware('auth')->only([
+            'create', 'store'
+        ]);*/
+
+        // Posso aplicar o middleware no contrutor para todo o controller, exceto para um metodo específico
+        $this->middleware('auth')->except([
+            'index', 'show'
+        ]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $products = ['Product 01', 'Product 02', 'Product 03'];
@@ -13,8 +41,69 @@ class ProductController extends Controller
         return $products;
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
         return "Exibindo o produto de id: {$id}";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return "Exibindo o form de cadastro de um novo produto";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "Form para editar o produto: {$id}";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return "Cadastrando um novo produto";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "Editando o produto: {$id}";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return "Deletando o produto: {$id}";
     }
 }
